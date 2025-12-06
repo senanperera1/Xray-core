29307a641b9a00b1394d84c2846374af51df6b9b
senanperera1 <senanperera5@gmail.com>
2025-12-06 20:57:35 +0530

    Update mobile.go

--- mobile/mobile.go
+++ mobile/mobile.go
@@ -1,1 +1,33 @@
+package mobile
 
+import (
+    "github.com/xtls/xray-core/core"
+)
+
+var instance *core.Instance
+
+// Start Xray with JSON config
+func Start(configJSON string) error {
+    conf, err := core.LoadConfigFromJson([]byte(configJSON))
+    if err != nil {
+        return err
+    }
+
+    inst, err := core.New(conf)
+    if err != nil {
+        return err
+    }
+
+    instance = inst
+    return instance.Start()
+}
+
+// Stop Xray
+func Stop() error {
+    if instance == nil {
+        return nil
+    }
+    err := instance.Close()
+    instance = nil
+    return err
+}
